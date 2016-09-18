957e41a0d5cd571a17903bfe9bd4965c94fee5c0
lromang <luis.roangarci@gmail.com>
2016-09-18 18:23:45 -0500

    access

--- class_and_send_urls.R
+++ class_and_send_urls.R
@@ -23,11 +23,12 @@ urls_code$slug <- str_trim(urls_code$slug)
 ## Función para clasificar una liga
 change_lable <- function(lable){
     class <- "no disponible"
-    if(str_detect(lable, "20[0-3]") |
-       str_detect(lable, "406")     |
-       str_detect(lable, "00.")     |
-       str_detect(lable, "30.")     |
-       str_detect(lable, "50.")
+    if( !str_detect(lable, "404")
+        ##str_detect(lable, "20[0-3]") |
+        ##str_detect(lable, "406")     |
+        ##str_detect(lable, "00.")     |
+        ##str_detect(lable, "30.")     |
+        ##str_detect(lable, "50.")
        ){
         class <- "disponible"
     }
@@ -46,13 +47,17 @@ urls_code$status <- change_lable_mult(urls_code$code)
 ### Incluir usuarios
 ###################################
 users    <- read.csv("users.csv", stringsAsFactors = FALSE)
-
 all_data <- merge(urls_code, users, by = "slug")
 
 ###################################
 ### Escribir resultados
 ###################################
 write.csv(all_data, "all_data_urls.csv", row.names = FALSE)
+
+###################################
+### Datos no disponibles
+###################################
+missing_data <- all_data[status == "no disponible",]
 
 ###################################
 ### Enviar resultados
@@ -68,7 +73,7 @@ send_ticket <- function(dep, urls, recs, dates, admi, idad, subid, email){
    )
 
     conc <- "\\n\\nLos errores pueden ser los siguientes:\\n\\n1.- El servidor no está disponible.\\n\\n2.- El recurso requiere derechos de acceso.\\n\\n3.- El servidor toma demasiado tiempo en responder a una solicitud por el recurso.\\n\\nAmablemente sugerimos atender dichas ligas y revisar todos los conjuntos de datos restantes que su dependencia publica en el sitio. Sin más por el momento, me mantengo a su disposición para resolver cualquier duda sobre el proceso de cumplimiento de la Política de Datos Abiertos en el correo escuadron@datos.gob.mx o vía telefónica al 50935300 ext: 7054.\\n\\nSaludos cordiales."
-    cl_curl <- paste0('"}, "status": "new", "type": "task", "priority": "normal", "tags": ["ligas_rotas"], "collaborator_ids":[',subid ,'] ,"requester": {"id":', idad,',"name":','"', admi,'", "email":', '"', email,'"}}', "}'",' -H "Content-Type: application/json" -v -u luis.roangarci@gmail.com:Ikidefenix131090 -X POST')
+    cl_curl <- paste0('"}, "status": "solved", "type": "task", "priority": "normal", "tags": ["ligas_rotas"], "collaborator_ids":[',subid ,'] ,"requester": {"id":', idad,',"name":','"', admi,'", "email":', '"', email,'"}}', "}'",' -H "Content-Type: application/json" -v -u email:password -X POST')
 
     all_text <- paste(op_curl, f_urls, conc, cl_curl, sep = "")
     ## writeLines(all_text, "test.txt")
@@ -84,10 +89,10 @@ send_multi_ticket <- function(all_data_non_disp){
         urls    <- send_to$url
         recs    <- send_to$rec
         dates   <- send_to$date
-        email   <- "andres@fractal.ai"
-        admi    <- "Andrés Gómez Urquiza"
-        idad    <- "1154750628"
-        subid   <- "1154750628"
+        email   <- "luis.roman@datos.mx"
+        admi    <- "Luis Manuel Román García"
+        idad    <- "1184046918"
+        subid   <- "1184046918"
         ## email   <- send_to$mail_adm[1]
         ## admi    <- send_to$administrador[1]
         ## idad    <- send_to$id_zen_adm[1]
@@ -96,9 +101,4 @@ send_multi_ticket <- function(all_data_non_disp){
     }
 }
 #######
-## Prueba
-#######
-data_test <- all_data[all_data$status != "disponible",]
-data_test <- data_test[data_test$code != "time_out",]
-data_test <- data_test[data_test$slug != "pemex",]
-send_multi_ticket(data_test)
+send_multi_ticket(missing_data)
